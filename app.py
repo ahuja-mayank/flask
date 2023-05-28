fdc8d70ddf03509ec5c9c811bf91e243bf1a8de5
ahuja-mayank <msrivas1@volvocars.com>
2023-05-28 16:39:04 +0530

    commit

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, Docker!'
+# @app.route('/')
+# def hello():
+#     return 'Hello, Docker!'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0')
+
+import os
+name = os.environ.get('NAME', 'Guest')
+print(f'Hello, {name}!')
